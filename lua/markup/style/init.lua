8181311806149bbc6ca46d294463c01a5014e073
Jean Giraldo <jeann.giraldo@gmail.com>
2025-08-13 08:37:35 -0500

    refactor: move "Markup" command entry point to init.lua

--- lua/markup/style/init.lua
+++ lua/markup/style/init.lua
@@ -1,33 +1,55 @@
 local modes = require("markup.style.modes")
 local utils = require("markup.style.utils")
 
+local Style = {}
+
+Style.OPTS = {
+	"bold",
+	"italic",
+	"bolditalic",
+	"code",
+	"quote",
+}
+
+local ERROR_LOG = vim.log.levels.ERROR
+
 ---Applies a text style either to a visual mode range or at the cursor in normal mode
 ---
 ---@param style_data table A table containing:
 ---   - opts table: Style options for the current filetype
----   - style string: Name of the style to apply (e.g., "bold", "italic")
+---   - choice string: Name of the style to apply (e.g., "bold", "italic")
 ---   - range? { start_line: integer, start_col: integer, end_line: integer, end_col: integer }
 ---     If omitted, the style is applied at the current cursor position in normal mode.
 ---
 --- @return nil
-return function(style_data)
-	local opts, style, range = style_data.opts, style_data.style, style_data.range
+function Style.start(style_data)
+	local opts, style_choice, range = style_data.opts, style_data.choice, style_data.range
 
+	if not style_choice then
+		vim.notify("No style selected", ERROR_LOG)
+		return
+	end
+	if not vim.list_contains(Style.OPTS, style_choice) then
+		vim.notify("No valid style selected", ERROR_LOG)
+		return
+	end
 	if not opts then
-		vim.notify("No style options detected for " .. vim.bo.filetype, vim.log.levels.WARN)
+		vim.notify("No style options detected for " .. vim.bo.filetype, ERROR_LOG)
 		return
 	end
 
-	local pattern = opts[style]
+	local pattern = opts[style_choice]
 	if not pattern then
-		utils.notify_error("No style options found", style)
+		utils.notify_error("No style options found", style_choice)
 		return
 	end
 
 	if range and range.start_line then
-		modes.visual(style, pattern, range)
+		modes.visual(style_choice, pattern, range)
 		return
 	end
 
-	modes.normal(style, pattern)
+	modes.normal(style_choice, pattern)
 end
+
+return Style
